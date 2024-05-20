2abd09ef0c9be07b2489a757afbcdabf1690bb8d
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-05-20 13:08:42 +0200

    conector

---
+++ repaso/src/main/java/connection/Config.java
@@ -0,0 +1,10 @@
+package connection;
+
+public interface Config {
+
+	public static final String BBDD = "caballeros";
+	public static final String HOST = "localhost";
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "";
+
+}
