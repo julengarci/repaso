3cfe4a01922f9c8092e551823a11215dbc720ebc
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-05-21 08:26:07 +0200

    caballero con arma y escudo

--- repaso/src/main/java/modeloCaballero/CaballeroModelo.java
+++ repaso/src/main/java/modeloCaballero/CaballeroModelo.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 import connection.Conector;
 import modeloArma.Arma;
+import modeloEscudo.Escudo;
 
 public class CaballeroModelo {
 
@@ -34,8 +35,11 @@ public class CaballeroModelo {
 				caballero.setExperiencia(rs.getInt(4));
 				caballero.setFoto(rs.getString(5));
 				
-				Arma arma = new Arma();
+				Arma arma = getArma(rs.getInt(6));
+				caballero.setArma(arma);
 				
+				Escudo escudo = getEscudo(rs.getInt(7));
+				caballero.setEscudo(escudo);
 				
 				caballeros.add(caballero);
 			}
@@ -43,6 +47,43 @@ public class CaballeroModelo {
 			System.out.println("error");
 		}
 		return caballeros;
+	}
+	
+	private Escudo getEscudo(int id) {
+		Escudo escudo = new Escudo();
+		try {
+			PreparedStatement pst = conector.getConexion().prepareStatement("SELECT * FROM ESCUDOS WHERE ID = ?");
+			pst.setInt(1, id);
+			ResultSet rs = pst.executeQuery();
+			
+			if (rs.next()) {
+				escudo.setId(rs.getInt(1));
+				escudo.setNombre(rs.getString(2));
+				escudo.setCapacidad_defensa(rs.getInt(3));
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return escudo;
+	}
+
+	public Arma getArma(int id) {
+		Arma arma = new Arma();
+		try {
+			PreparedStatement pst = conector.getConexion().prepareStatement("SELECT * FROM ARMAS WHERE id = ?");
+			pst.setInt(1, id);
+			ResultSet rs = pst.executeQuery();
+			
+			if (rs.next()) {
+				arma.setId(rs.getInt(1));
+				arma.setNombre(rs.getString(2));
+				arma.setCapacidad_danio(rs.getInt(3));
+				arma.setFoto(rs.getString(4));
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return arma;
 	}
 	
 }
